a00edca2afb9292e502e0a884ff11bee7d629ff5
guomin <yushi5344@gmail.com>
2017-12-23 20:41:43 +0800

    for循环

---
+++ for.py
@@ -0,0 +1,9 @@
+# Author guomin
+# for i in range(1,10):
+#     print("loop",i)
+for i in range(10):
+    print('-----',i)
+    for j in range(10):
+        print(j)
+        if j>5:
+            break
