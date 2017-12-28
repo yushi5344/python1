2163b046069d7d14880ca23cb760d4db88f13f5f
guomin <yushi5344@gmail.com>
2017-12-28 22:10:27 +0800

    文件操作

--- file_opt.py
+++ file_opt.py
@@ -7,10 +7,21 @@
 # b.write("天安门上太阳升")
 # c=open("test",'a',encoding="utf-8")
 # c.write("when i was young ")
-f=open('test','r',encoding="utf-8")
+#f=open('test','r+',encoding="utf-8")
 # for index,line in enumerate(f.readlines()):
 #     print(line.strip())
 #     if index>9:
 #         break;
-for line in f:
-    print(line)
+# for line in f:
+#     print(line)
+#ileno())#文件在内存中的编号
+import sys,time
+# for i in range(20):
+#     sys.stdout.write('#')
+#     sys.stdout.flush()#内存中刷新输出
+#     time.sleep(0.5)
+#f.truncate(20)
+#f=open('test','W+',encoding='utf-8')#写读  先写后读
+f=open('test3','wb')
+f.write('Hello world'.encode())
+f.close()
