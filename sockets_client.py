7e19ff36280cd4ffe936380c38265ae5e5ba197a
guomin <yushi5344@gmail.com>
2018-05-06 14:36:47 +0800

    python 中的socket多次通信

--- sockets_client.py
+++ sockets_client.py
@@ -3,7 +3,10 @@ import socket
 #这是客户端
 client=socket.socket() #生成socket类型，同时生成socket连接对象
 client.connect(('localhost',6969))
-client.send(b'Hello World')
-data=client.recv(1024)
-print("rece",data)
+while True:
+    msg=input('>>:').strip()
+    client.send(msg.encode('utf-8'))
+    data=client.recv(1024)
+    print("rece",data.decode())
+
 client.close()
--- sockets_server.py
+++ sockets_server.py
@@ -5,9 +5,11 @@ server.bind(('localhost',6969))
 server.listen()
 print('我要开始等电话了')
 conn,addr=server.accept() #等待
-#conn就是客户端连过来而在服务器端生成的一个连接实例
+# conn就是客户端连过来而在服务器端生成的一个连接实例
 print('等到了')
-data=conn.recv(1024)
-print('rece_server',data)
-conn.send(data.upper())
+while True:
+    data=conn.recv(1024)
+    print('rece_server',data)
+    conn.send(data.upper())
+
 server.close()
